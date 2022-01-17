5b9af8df5e6ec5d6828cb10dd449048e7c51b542
TheWizardXM <67564351+TheWizardXM@users.noreply.github.com>
2022-01-17 19:51:03 +0200

    Add files via upload

---
+++ tast150.py
@@ -0,0 +1,10 @@
+def mul(x,y):
+    i = int(input('enter oprastion :'))
+    if (i == 1):
+        print(x*y)
+    elif i == 2:
+        print(x/y)
+    elif i == 3 :
+        print(x-y)
+    else:
+        print(x+y)
---
+++ test151.py
@@ -0,0 +1,13 @@
+from tast150 import mul
+
+x = int(input('enter first number : '))
+y = int(input('enter sacand number : '))
+
+mul(x,y)
+n = 1
+while n == 1:
+    print("if you need contano chose 2 :")
+    n = int(input('enter chose : '))
+    x = int(input('enter first number ; '))
+    y = int(input('enter sacand number ; '))
+    mul(x,y)
